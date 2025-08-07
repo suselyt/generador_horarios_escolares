34e36f5800c4cb252cf2589486f7b2f6d40389c8
Susely <susely.dev@gmail.com>
2025-08-06 22:37:18 -0500

    programa con conexion a db en cluster de mongodb

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 async function conectarDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/horariosDB', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        await mongoose.connect(process.env.MONGO_URI, {
         });
-        console.log('CONNNECTEADO A MONGODB')
+        console.log('CONNNECTEADO A MONGODB ATLAS')
     }
     catch {
         console.error('ERRORRR CON CONEXION:', error);
