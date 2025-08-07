b6e367bb8ff4468e0d9af5cf0a768dd2835cc42d
Susely <susely.dev@gmail.com>
2025-08-06 20:32:49 -0500

    elimine modelo que no se usara, todo listo

--- models/horario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const HorarioSchema = new mongoose.Schema({
-    fechaExportacion: { type: Date, default: Date.now },
-    horariosProfesores: { type: Object, required: true },
-    horariosGrupales: { type: Object, required: true }
-})
-
-module.exports = mongoose.model("Horario", HorarioSchema);
